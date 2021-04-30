00a3d79332c2f0d510c301e0ce3c9324a133206e
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-04-30 14:32:44 +0200

    Surname Uppercase

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 // USER SURNAME REQUEST
 var userSurname = prompt("Write your surname, please.");
+var userSurname = userSurname[0].toUpperCase() + userSurname.substring(1);
 
 // USER SURNAME IN [SURNAMES LIST] ARRAY
-var surnamesList = ["White", "Black", "Red", "Green", "Yellow", "Orange", "Violet", "Brown", "Purple", "Pink"];
+var surnamesList = ["Rossi", "Ferrari", "Russo", "Bianchi", "Gallo", "Costa", "Fontana", "Conti", "Esposito", "Ricci", "De Luca", "Greco", "Barbieri", "Conte", "Marini", "Gatti", "Caruso"];
 
 surnamesList.push(userSurname);
 
 // PRINT OF THE ORDERED [SURNAMES LIST]
 surnamesList.sort();
 
-var surnamesListUl = document.getElementById("surnames_list");
+var surnamesListOl = document.getElementById("surnames_list");
 
 for (var i=0 ; i < surnamesList.length ; i++) {
-    surnamesListUl.innerHTML += "<li>" + surnamesList[i] + "</li>";
+    surnamesListOl.innerHTML += "<li> " + surnamesList[i] + "</li>";
 }
 
 // USER'S SURNAME POSITION IN THE [SURNAMES LIST]
-
 var surnamePositionP = document.getElementById("user_surname_position");
 var surnamePosition = 1;
 
