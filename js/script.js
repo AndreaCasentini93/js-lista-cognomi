c0ab0f36f5a89f53688a93311f40ad5cd9bbf756
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-04-30 13:39:05 +0200

    3. Point

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
-// 1. chiedi all'utente il cognome
+// 1. ask the user for their surname
+var userSurname = prompt("Write your surname, please.");
 
+// 2. insert it in an array with other surnames: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+var surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
-// 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+surnameList.push(userSurname);
 
+// 3. print the list sorted alphabetically
+surnameList.sort();
 
-// 3. stampa la lista ordinata alfabeticamente
+var surnameListUl = document.getElementById("surname_list");
 
+for (var i=0 ; i < surnameList.length ; i++) {
+    surnameListUl.innerHTML += "<li>" + surnameList[i] + "</li>";
+}
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 
+var surnamePositionP = document.getElementById("user_surname_position");
+var surnamePosition = 0;
+
+var test = true;
+
+i = 0
+while ((i < surnameList.length) && (test == false)) {
+    
+
+    i++;
+}
