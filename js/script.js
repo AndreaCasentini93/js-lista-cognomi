21abc8ecc9af37229c3b1c5f335c15e3b9b55614
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-04-30 16:41:05 +0200

    Calculator doubleClick NO

--- js/script.js
+++ js/script.js
@@ -14,52 +14,48 @@ var doubleClick = true;
 
 function calculator() {
 
-    if (doubleClick) {
+    // RESET DELLA LISTA COGNOMI
+    surnamesListOl.innerHTML = "";
 
-        // RESET DELLA LISTA COGNOMI
-        surnamesListOl.innerHTML = "";
+    // CHIEDI ALL'UTENTE IL SUO COGNOME
+    var userSurnameInput = document.getElementById("user_surname");
+    var userSurname = userSurnameInput.value;
+    var userSurname = userSurname[0].toUpperCase() + userSurname.substring(1);
 
-        // CHIEDI ALL'UTENTE IL SUO COGNOME
-        var userSurnameInput = document.getElementById("user_surname");
-        var userSurname = userSurnameInput.value;
-        var userSurname = userSurname[0].toUpperCase() + userSurname.substring(1);
+    // INSERISCI IL COGNOME DELL'UTENTE NELLA LISTA
+    surnamesList.push(userSurname);
 
-        // INSERISCI IL COGNOME DELL'UTENTE NELLA LISTA
-        surnamesList.push(userSurname);
+    // STAMPA LA LISTA IN ORDINE ALFABETICO
+    surnamesList.sort();
 
-        // STAMPA LA LISTA IN ORDINE ALFABETICO
-        surnamesList.sort();
-
-        for (var i=0 ; i < surnamesList.length ; i++) {
-            surnamesListOl.innerHTML += "<li>" + surnamesList[i] + "</li>";
-        }
-
-        // SPECIFICA LA POSIZIONE NUMERICA DEL COGNOME UTENTE NELLA LISTA
-        var surnamePosition = 1;
-
-        var test = true;
-
-        i = 0
-        while ((i < surnamesList.length) && (test)) {
-            
-            if (userSurname == surnamesList[i]) {
-                surnamePosition += i;
-                test = false;
-            }
-
-            i++;
-        }
-
-        var surnamePositionP = document.getElementById("user_surname_position");
-        surnamePositionP.innerHTML = "Your surname has position number " + surnamePosition + " in the surnames list.";
-
-        // PULISCI L'INPUT TEXT
-        userSurnameInput.value = "";
-
-        // BLOCCA IL DOPPIO CLICK DEL PULSANTE
-        doubleClick = false;
-
+    for (var i=0 ; i < surnamesList.length ; i++) {
+        surnamesListOl.innerHTML += "<li>" + surnamesList[i] + "</li>";
     }
+
+    // SPECIFICA LA POSIZIONE NUMERICA DEL COGNOME UTENTE NELLA LISTA
+    var surnamePosition = 1;
+
+    var test = true;
+
+    i = 0
+    while ((i < surnamesList.length) && (test)) {
+        
+        if (userSurname == surnamesList[i]) {
+            surnamePosition += i;
+            test = false;
+        }
+
+        i++;
+    }
+
+    var surnamePositionP = document.getElementById("user_surname_position");
+    surnamePositionP.innerHTML = "Your surname has position number " + surnamePosition + " in the surnames list.";
+
+    // PULISCI L'INPUT TEXT
+    userSurnameInput.value = "";
+
+    // BLOCCA IL DOPPIO CLICK DEL PULSANTE
+    doubleClick = false;
 
 }
 
